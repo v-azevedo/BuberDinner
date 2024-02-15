9547b52c5f0a76855126683b644721a06b5bf1ff
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-02-15 14:55:14 -0300

    Update response with jwt generated token

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         var response = new AuthenticationResult(
@@ -17,12 +26,16 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        var userid = Guid.NewGuid();
+
+        var token = _jwtTokenGenerator.TokenGenerator(userid, firstName, lastName);
+
         var response = new AuthenticationResult(
             Guid.NewGuid(),
             firstName,
             lastName,
             email,
-            "token"
+            token
         );
 
         return response;
