c1d75f2c661be660b0bc4a62bfb01a947cba7f11
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-02-20 21:25:26 -0300

    Add command for authentication register

---
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,44 @@
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public RegisterCommandHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _userRepository = userRepository;
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        if (_userRepository.GetUserByEmail(request.Email) is not null)
+            return Errors.User.DuplicateEmail;
+
+        var user = new User
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Password = request.Password
+        };
+
+        _userRepository.AddUser(user);
+
+        var token = _jwtTokenGenerator.TokenGenerator(user.Id, request.FirstName, request.LastName);
+
+        return new AuthenticationResult(
+            user,
+            token
+        );
+    }
+}
