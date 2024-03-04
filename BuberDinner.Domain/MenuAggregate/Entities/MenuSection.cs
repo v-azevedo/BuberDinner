486fe67ad10d573d7eadc69a33112136af51a180
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-03-04 16:11:22 -0300

    Remove unused list variable

--- BuberDinner.Domain/MenuAggregate/Entities/MenuSection.cs
+++ BuberDinner.Domain/MenuAggregate/Entities/MenuSection.cs
@@ -5,7 +5,6 @@ namespace BuberDinner.Domain.MenuAggregate.Entities;
 
 public sealed class MenuSection : Entity<MenuSectionId>
 {
-    private readonly List<MenuItem> _items = new();
     public string Name { get; }
     public string Description { get; }
     public IReadOnlyList<MenuItem> Items { get; }
